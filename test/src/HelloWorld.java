24c5d63ca90ba5b9d296a92112e364ce72c01a0f
lizhishang <zhishanglife@gmail.com>
2019-01-26 16:37:41 +0800

    First time add the code to github

---
+++ test/src/HelloWorld.java
@@ -0,0 +1,9 @@
+/* *
+ * It's my first time to upload the code.
+ * */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Github");
+    }
+}
